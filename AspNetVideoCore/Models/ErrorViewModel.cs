f703fa1b36dfd9f7949d23c8b79f02dc02d24154
JeffWhitman <67132067+JeffWhitman@users.noreply.github.com>
2020-06-24 19:23:38 -0500

    Add project files.

---
+++ AspNetVideoCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetVideoCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
